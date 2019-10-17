1ea51184f664069efb3137d83e84e9e0080ad61d
M Powers <mp@mpowers.info>
2019-10-17 13:11:31 -0400

    Initial models, 'index' view

--- app/roadfoodr/rftrackr/models.py
+++ app/roadfoodr/rftrackr/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+	user_id = models.AutoField(primary_key=True)
+	name_first = models.CharField(max_length=40)
+	name_last = models.CharField(max_length=40)
+	pub_date = models.DateTimeField('date published')
+
+class Roadfoods(models.Model):
+	rf_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=60)
+
+class Visits(models.Model):
+	user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+	rf_id = models.ForeignKey(Roadfoods, on_delete=models.CASCADE)
+	visit_date = models.DateTimeField('date visited')
+
--- app/roadfoodr/rftrackr/urls.py
+++ app/roadfoodr/rftrackr/urls.py
@@ -1,4 +1,4 @@
-"""roadfoodr URL Configuration
+""" URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('rftrackr.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+#    path('admin/', admin.site.urls),
 ]
